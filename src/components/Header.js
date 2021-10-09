c6ad5565e2db21180f5ff0fe3f43adeba62fcb91
abnvsingh62 <abnvsingh62@gmail.com>
2021-10-09 22:29:08 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return(
+            <header className="header">
+                <h1>
+                    Emoji Search By Abhinav Kumar Singh
+                </h1>
+            </header>
+        );
+    }
+}
+
+export default Header
